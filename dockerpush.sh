6460f692ab668abe08d9463a0b51935b7ccc3f04
Christian Steinmann <christian.steinmann77@gmail.com>
2015-07-25 15:23:42 +0200

    Added env file

--- dockerpush.sh
+++ dockerpush.sh
@@ -12,9 +12,10 @@ Also it must be allowed to call git init and docker-compose up -d and docker-com
 
 Set environment DOCKERPUSH_WORKDIR to a path where the work dirs should be placed else it defaults to /var/www/repos
 
+[ENVFILE] Set the path to an file. this file will be copied to the workdir root.
 Usage:
 
-dockerpush.sh reponame [branch=master]
+dockerpush.sh reponame [ENVFILE]
 ';
 
 REPONAME=$1
@@ -31,9 +32,10 @@ fi
 #############################################################################################################
 if [ $# -eq 2 ];
 then
-    BRANCH=$2
+    ENVFILE=$2
+    touch ENVFILE
 else
-    BRANCH=master
+    ENVFILE=""
 fi
 
 CURRENTDIR="$PWD"
@@ -70,13 +72,15 @@ cat <<EOF > post-receive
 while read oldrev newrev ref
 do
     echo "Post Receive Hook ..."
-    if [[ \$ref =~ .*/$BRANCH$ ]];
+    if [[ \$ref =~ .*/master$ ]];
     then
-        echo "$BRANCH ref received.  Deploying $BRANCH branch to production..."
+        echo "master ref received.  Deploying master branch to production..."
         git --work-tree="$WORKTREE" --git-dir="$GITDIR" checkout -f
         if [ -f "$WORKTREE/docker-compose.yml" ];
         then
-            source ~/.bashrc
+            if [ -f "$ENVFILE" ];then
+                cp "$ENVFILE" "$WORKTREE/"
+            fi
             cd $WORKTREE
             sudo -E docker-compose build
             sudo -E docker-compose up -d
@@ -84,7 +88,7 @@ do
              echo "Could not find docker-compose.yml"
         fi
     else
-        echo "Ref \$ref successfully received.  Doing nothing: only the $BRANCH branch may be deployed on this Repo."
+        echo "Ref \$ref successfully received.  Doing nothing: only the master branch may be deployed on this Repo."
         exit 1
     fi
 done
