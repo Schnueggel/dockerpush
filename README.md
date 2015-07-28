12fe19b03897b9c099e7d74ccbfe183af0454297
Christian Steinmann <christian.steinmann77@gmail.com>
2015-07-28 02:24:00 +0200

    More security

--- README.md
+++ README.md
@@ -8,6 +8,8 @@ The public ssh key of your gituser must be placed in the authorized keys file.
 
 Docker and docker-compose must be installed.
 
+sudo 
+
 See also [Git Hook](#githook)
 
 ## Usage
@@ -19,15 +21,15 @@ Place this script in your git users home folder and make it executable:
 
 Then call:
 
-```sudo ./dockerpush.sh yourreponame gituser composerenvfile.env```
+```sudo ./dockerpush.sh myreponame gituser -e composerenvfile.env```
 
 This script must be called with sudo or as root.
 
-This will create a new bare repository with the name "reponame" in the current folder and extend the folder with .git. 
-If the repo exist it will be destroyed and recreated. Example:
+This will create a new bare repository with the name "myreponame.git" in the current folder. 
+If the repository exist it will be destroyed and recreated. Example:
 
 ```
-cd ~ 
+cd /home/gituser 
 ```
 
 ```
@@ -39,7 +41,7 @@ chmod u+x ./dockerpush.sh
 ```
 
 ```
-sudo ./dockerpush.sh myapp gituser myapp.env
+sudo ./dockerpush.sh myapp gituser -e myapp.env
 ```
 
 Will generate a folder:
@@ -61,7 +63,7 @@ And the worktree dir:
 ```
 
 Later when you push into the repository the myapp.env will be symlinked into the worktree dir:
- 
+
 -- var
 
 ---- www
@@ -71,6 +73,14 @@ Later when you push into the repository the myapp.env will be symlinked into the
 -------- myapp
 
 ---------- myapp.env
+
+The worktree and the data inside cannot be modified or read by the git user
+
+
+### dockerpush-strategy.sh
+
+This file will be generated on the first run of dockerpush.sh inside the .dockerpush directory which will also generated.
+Those files cannot be read or modified by the gituser
 
 ### EnvFile
 
@@ -90,7 +100,7 @@ The generated git hook calls ```docker-compose build``` and ```docker-compose up
 #### Prequesite
 To make this work type ```visudo``` and add the following line for example:
 
-```gituser ALL=(root) NOPASSWD: /usr/local/bin/docker-compose```
+```gituser ALL=(root) NOPASSWD: /home/gituser/.dockerpush/dockerpush-strategy.sh```
 
 ### Git push
 
@@ -103,11 +113,12 @@ Add a new remote repository destination to your local repository with:
 git remote add deploy repouser@myrepos.com
 ```
 
-Create a commit:
+Create a commit ( Add a file version txt file or something):
 
 ```
-git commit --allow-empty -m "New Version"
+git commit -m "New Version"
 ```
+--allow-empty flag on commits will fail when pushed to an empty remote repo
 
 Next deploy  what ever local branch you are to the master branch of your repo
 ```
@@ -123,3 +134,5 @@ Be aware of your gitingore file. gitignore files are global and not per branch. 
 ### TODO
 
 Lock if docker-compose is already running on multiple pushs?
+
+Prompt before delete of directories
--- dockerpush.sh
+++ dockerpush.sh
@@ -3,119 +3,246 @@ set -e
 
 : ${DOCKERPUSH_WORKDIR:=/var/www/repos}
 
+while getopts ":e:s:" o; do
+    case "${o}" in
+        e)
+            e=${OPTARG}
+            ;;
+        s)
+            s=${OPTARG}
+            ;;
+    esac
+done
+
+#############################################################################################################
+# Script must be executed as superuser
+#############################################################################################################
 if ! [ $(id -u) = 0 ]; then
    echo "This script must be called as root or with sudo"
    exit 1
 fi
 
 #############################################################################################################
-# HELP TEXT
+# Setup vars
 #############################################################################################################
-HELP='
-This script must be allowed to create files in this dir and in the dockerpush worktree dir.
-Also it must be allowed to call git init and docker-compose up -d and docker-compose build
+NAME="$1"
+USER="$2"
+CURRENTDIR="$PWD"
+REPONAME="$NAME.git"
+WORKTREE="$DOCKERPUSH_WORKDIR"/$NAME
+GITDIR="$CURRENTDIR/$REPONAME"
+DOCKERPUSH_DIR=".dockerpush"
+DOCKERPUSH_REPO=$DOCKERPUSH_DIR/$NAME
+HOOKFILE=$REPONAME/hooks/post-receive
+LOGENVFILE=$DOCKERPUSH_REPO/envfile
 
-Set environment DOCKERPUSH_WORKDIR to a path where the work dirs should be placed else it defaults to /var/www/repos
-
-[ENVFILE] Set the path to an file. this file will be copied to the workdir root.
-
-Usage:
-
-dockerpush.sh reponame gituser [ENVFILE]
-';
-
-REPONAME=$1
 #############################################################################################################
 # Display help if help flag is set or number of arguments is wrong
 #############################################################################################################
-if [[ $REPONAME == "--help" ]] || [ "$#" -lt "2" ] || [ "$#" -gt "3" ]; then
-    echo "$HELP"
+if [[ $NAME == "--help" ]] || [ "$#" -lt "2" ] || [ "$#" -gt "3" ]; then
+    showHelp
     exit 0
 fi
 
+#############################################################################################################
+# Check if the given user does exist
+#############################################################################################################
 if ! id -u "$2" >/dev/null 2>&1; then
     echo "The given user $2 does not exist"
     exit 1
 fi
+
+#############################################################################################################
+# Create all this stuff. Be careful with the order of execution
+#############################################################################################################
+createEnvFile
+createDockerpushDir
+createWorkTreeDir
+createRepo
+createHook
+createDefaultStrategy
+
 #############################################################################################################
 # Set branch if given else set to master
 #############################################################################################################
-if [ $# -eq 3 ];
-then
-    #############################################################################################################
-    # Create env file and make it readable only to root. docker-compose can read it
-    #############################################################################################################
-    touch $3
-    chmod o-r $3
-    ENVFILE=$(readlink -f $3)
-    ENVBASE=$(basename $ENVFILE)
-else
-    ENVFILE=""
-fi
-USER=$2
-CURRENTDIR="$PWD"
-REPONAME="$1.git";
-WORKTREE="$DOCKERPUSH_WORKDIR/$1"
-GITDIR="$CURRENTDIR/$REPONAME"
 
-if [ -d "$REPONAME" ]; then
-    rm -rf $REPONAME;
-fi
 
-if [ -d "$WORKTREE" ]; then
-    rm -rf $WORKTREE;
-fi
-
-mkdir -p $WORKTREE
-chown -R "$USER":"$USER" "$WORKTREE"
-
-#############################################################################################################
-# Create bare repo without work tree.
-#############################################################################################################
-
-mkdir "$REPONAME"
-
-cd "$REPONAME";
-
-git init --bare
-
-cd hooks;
-
+function createEnvFile {
+    if [ $e ];
+    then
+        #############################################################################################################
+        # Create env file and make it readable only to root. docker-compose can read it
+        #############################################################################################################
+        touch "$e"
+        # Get full path
+        ENVFILE=$(readlink -f $e)
+        echo $ENVFILE > $LOGENVFILE
+        chmod 640 $LOGENVFILE
+        chmod 640 $ENVFILE
+    else
+        ENVFILE=""
+    fi
+}
 #############################################################################################################
 # Create hook script
 #############################################################################################################
-cat <<EOF > post-receive
-#!/bin/sh
+function createHook {
 
-while read oldrev newrev ref
-do
-    echo "Post Receive Hook ..."
-    if [[ \$ref =~ .*/master$ ]];
-    then
-        echo "master ref received.  Deploying master branch to production..."
-        git --work-tree="$WORKTREE" --git-dir="$GITDIR" checkout -f
-        if [ -f "$WORKTREE/docker-compose.yml" ];
-        then
-            if [ -f "$ENVFILE" ];then
-                ln -sf "$ENVFILE" "$WORKTREE/$ENVBASE"
+    cat <<-EOF > $HOOKFILE
+        #!/bin/bash
+
+        while read oldrev newrev ref
+        do
+            echo "Post Receive Hook ..."
+            if [[ \$ref =~ .*/master$ ]];
+            then
+                echo "master ref received.  Deploying master branch to production..."
+                sudo ./dockerpush-strategy.sh "$NAME"
+                exit 0
+            else
+                echo "Ref \$ref successfully received.  Doing nothing: only the master branch may be deployed on this Repo."
             fi
-            cd $WORKTREE
-            sudo docker-compose build
-            sudo docker-compose up -d
+            exit 1
+        done
+EOF
+    chmod +x $HOOKFILE
+}
+
+
+#############################################################################################################
+# HELP TEXT
+#############################################################################################################
+
+function showHelp {
+    echo '
+    This script must be allowed to create files in this dir and in the dockerpush worktree dir.
+    Also it must be allowed to call git init and docker-compose up -d and docker-compose build
+
+    Set environment DOCKERPUSH_WORKDIR to a path where the work dirs should be placed else it defaults to /var/www/repos
+
+    [ENVFILE] Set the path to an file. This file will be symlinked to the workdir root.
+
+    Usage: dockerpush.sh reponame gituser [-e envfile] [-s strategyfile]
+    '
+}
+
+#############################################################################################################
+# Create the dockerpush dir if it does not exist and creagted the repo name. This will delete the old repo
+#############################################################################################################
+function createDockerpushDir {
+    if [ ! -d $DOCKERPUSH_DIR ];then
+        mkdir  $DOCKERPUSH_DIR
+        chmod 640 $DOCKERPUSH_DIR
+    fi
+    if [ -d $DOCKERPUSH_REPO ]; then
+        rm -rf $DOCKERPUSH_REPO
+    fi
+    mkdir $DOCKERPUSH_REPO
+    chmod 640 $DOCKERPUSH_REPO
+}
+
+#############################################################################################################
+# Creates the worktree dir
+#############################################################################################################
+function createWorkTreeDir {
+
+    if [ -d $WORKTREE ]; then
+        rm -rf $WORKTREE;
+    fi
+
+    mkdir -p $WORKTREE
+    chmod 640 $WORKTREE
+}
+#############################################################################################################
+# Create the repo folder and inits a bare repository
+#############################################################################################################
+function createRepo {
+    if [ -d $REPONAME ]; then
+        rm -rf $REPONAME;
+    fi
+    mkdir $REPONAME
+
+    git init --bare $REPONAME
+    chown -R $USER:$USER $REPONAME
+}
+#############################################################################################################
+# Create Strategy Script
+#############################################################################################################
+function createDefaultStrategy {
+    if [ -f "$DOCKERPUSH_DIR/dockerpush-strategy.sh" ]; then
+        return
+    fi
+    cat <<-EOF > "$DOCKERPUSH_DIR/dockerpush-strategy.sh"
+        #!/bin/bash
+        #############################################################################################################
+        # This script must be called with sudo
+        # usage: ./dockerpush-strategy.sh reponame
+        #
+        # reponame without .git extension
+        #############################################################################################################
+
+        #############################################################################################################
+        # Script must be executed as superuser
+        #############################################################################################################
+        if ! [ $(id -u) = 0 ]; then
+           echo "This script must be called as root or with sudo"
+           exit 1
+        fi
+
+        #############################################################################################################
+        # Vars
+        #############################################################################################################
+        : \${DOCKERPUSH_WORKDIR:=/var/www/repos}
+
+        NAME="\$1"
+        REPONAME=\$NAME.git
+        WORKTREE="\$DOCKERPUSH_WORKDIR/\$NAME"
+        GITDIR=$CURRENTDIR/\$REPONAME
+        DOCKERPUSH_REPO=$DOCKERPUSH_DIR/\$NAME
+        LOGENVFILE=\$DOCKERPUSH_REPO/envfile
+
+        #############################################################################################################
+        # Check for a valid repo created with the dockerpush script
+        #############################################################################################################
+        if [ -d \$DOCKERPUSH_REPO ]; then
+            echo "[ERROR] You are not allowed use this strategy on the repository \$NAME"
+            exit 1
+        fi
+
+        #############################################################################################################
+        # Check if a lock file was written
+        #############################################################################################################
+        if [ -f \$LOGENVFILE ]; then
+            ENVFILE=\$(read -r FIRSTLINE < \$LOGENVFILE)
+            WROKTREE_ENV=\$WORKTREE/\$(basename \$ENVFILE)
+        else
+            ENVIFLE=
+        fi
+
+        #############################################################################################################
+        # Checkout git repo
+        #############################################################################################################
+        git --work-tree=\$WORKTREE --git-dir=\$GITDIR checkout -f
+
+        #############################################################################################################
+        # Execute docker-compose
+        #############################################################################################################
+        if [ -f \$WORKTREE/docker-compose.yml ];
+        then
+            #############################################################################################################
+            # Link env file in to worktree forceful
+            #############################################################################################################
+            if [ -f \$ENVFILE ];then
+                ln -sf \$ENVFILE \$WORKTREE_ENV
+            fi
+            cd \$WORKTREE
+            docker-compose build
+            docker-compose up -d
             exit 0
         else
              echo "Could not find docker-compose.yml"
+             exit 1
         fi
-    else
-        echo "Ref \$ref successfully received.  Doing nothing: only the master branch may be deployed on this Repo."
-    fi
-    exit 1
-done
 EOF
-
-chmod +x post-receive
-
-cd ..
-cd ..
-
-chown -R "$USER":"$USER" "$REPONAME"
+    chmod 640 dockerpush-strategy.sh
+}
